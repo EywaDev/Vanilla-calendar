1995ed8f7537c042abf58c277a6faba8c8e4facc
Lennert <lennertj@forwrd.be>
2024-04-26 12:03:15 +0200

    added working demo for the features we plan to use

--- demo/main.ts
+++ demo/main.ts
@@ -6,16 +6,70 @@ import { IOptions } from '@package/types';
 import VanillaCalendar from '@src/vanilla-calendar';
 import '@src/styles/vanilla-calendar.css';
 
-const config: IOptions = {
-	settings: {
-		selected: {
-			month: 3,
-			year: 2023,
-		},
-	},
+let config: IOptions;
+
+
+//multiple-range
+config = {
+    date: {
+        min: '2024-04-12',
+        max: '2024-06-15',
+    },
+    type: 'multiple',
+    settings: {
+        selection: {
+            day: 'multiple-ranged',
+            time: 24,
+        },
+        selected: {
+            time: ['10:20', '11:45']
+        },
+        visibility: {
+            weekend: false,
+            theme: 'light'
+        }
+    },
+    actions: {
+        clickDay(event, self) {
+            console.log(self.selectedDates);
+        },
+        changeTime(event, self) {
+            console.log(self.selectedTime);
+        }
+    },
 };
 
+
+//single
+config = {
+    date: {
+        min: '2024-04-12',
+        max: '2024-06-15',
+    },
+    settings: {
+        selection: {
+            time: 24,
+        },
+        selected: {
+            time: ['10:20']
+        },
+        visibility: {
+            weekend: false,
+            theme: 'light'
+        }
+    },
+    actions: {
+        clickDay(event, self) {
+            console.log(self.selectedDates);
+        },
+        changeTime(event, self) {
+            console.log(self.selectedTime);
+        }
+    },
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
-	const calendar = new VanillaCalendar('#calendar', config);
-	calendar.init();
+    const calendar = new VanillaCalendar('#calendar', config);
+    calendar.init();
 });
--- package/src/scripts/methods/createTime.ts
+++ package/src/scripts/methods/createTime.ts
@@ -2,7 +2,7 @@ import VanillaCalendar from '@src/vanilla-calendar';
 import transformTime24 from '@scripts/helpers/transformTime24';
 import changeTime from '@scripts/methods/changeTime';
 
-export const InputTime = (name:string, CSSClass: string, value: string, range: boolean) => (`
+export const InputTime = (name: string, CSSClass: string, value: string, range: boolean) => (`
 	<label class="${CSSClass}">
 		<input type="text"
 			name="${name}"
@@ -24,30 +24,50 @@ export const RangeTime = (name: string, CSSClass: string, min: number, max: numb
 `);
 
 const createTime = (self: VanillaCalendar) => {
-	const timeEl: HTMLElement | null = self.HTMLElement.querySelector(`.${self.CSSClasses.time}`);
-	if (!timeEl) return;
+    const timeEl: HTMLElement | null = self.HTMLElement.querySelector(`.${self.CSSClasses.time}`);
+    if (!timeEl) return;
 
-	const keepingTime = self.settings.selection.time === true ? 12 : self.settings.selection.time;
-	const range = self.settings.selection.controlTime === 'range';
-	const [minHour, maxHour] = [0, 23];
-	const [minMinutes, maxMinutes] = [0, 59];
+    const keepingTime = self.settings.selection.time === true ? 12 : self.settings.selection.time;
+    const range = self.settings.selection.controlTime === 'range';
+    const [minHour, maxHour] = [0, 23];
+    const [minMinutes, maxMinutes] = [0, 59];
 
-	timeEl.innerHTML = (`
+    if (self.settings.selection.day === 'multiple' || self.settings.selection.day === 'multiple-ranged') {
+        timeEl.innerHTML = (`
 		<div class="${self.CSSClasses.timeContent}">
-			${InputTime('hours', self.CSSClasses.timeHours, self.selectedHours, range)}
-			${InputTime('minutes', self.CSSClasses.timeMinutes, self.selectedMinutes, range)}
+			${InputTime('hours', self.CSSClasses.timeHours, self.selectedHours[0], range)}
+			${InputTime('minutes', self.CSSClasses.timeMinutes, self.selectedMinutes[0], range)}
 			${keepingTime === 12 ? `
 			<button type="button" class="${self.CSSClasses.timeKeeping}"
 				${range ? 'disabled' : ''}>${self.selectedKeeping}</button>` : ''}
 		</div>
 		<div class="${self.CSSClasses.timeRanges}">
-			${RangeTime('hours', self.CSSClasses.timeRange, minHour, maxHour, self.settings.selection.stepHours, self.selectedKeeping
-			? transformTime24(self.selectedHours, self.selectedKeeping) : self.selectedHours)}
-			${RangeTime('minutes', self.CSSClasses.timeRange, minMinutes, maxMinutes, self.settings.selection.stepMinutes, self.selectedMinutes)}
 		</div>
+        <div class="${self.CSSClasses.timeContent}">
+        ${InputTime('hoursEnd', self.CSSClasses.timeHours, self.selectedHours[1], range)}
+        ${InputTime('minutesEnd', self.CSSClasses.timeMinutes, self.selectedMinutes[1], range)}
+        ${keepingTime === 12 ? `
+        <button type="button" class="${self.CSSClasses.timeKeeping}"
+            ${range ? 'disabled' : ''}>${self.selectedKeeping}</button>` : ''}
+    </div>
 	`);
+    } else {
+        timeEl.innerHTML = (`
+            <div class="${self.CSSClasses.timeContent}">
+                ${InputTime('hours', self.CSSClasses.timeHours, self.selectedHours[0], range)}
+                ${InputTime('minutes', self.CSSClasses.timeMinutes, self.selectedMinutes[0], range)}
+                ${keepingTime === 12 ? `
+                <button type="button" class="${self.CSSClasses.timeKeeping}"
+                    ${range ? 'disabled' : ''}>${self.selectedKeeping}</button>` : ''}
+            </div>
+            <div class="${self.CSSClasses.timeRanges}">
+            </div>`
+        )
+    }
 
-	changeTime(self, timeEl, keepingTime);
+
+
+    changeTime(self, timeEl, keepingTime);
 };
 
 export default createTime;
